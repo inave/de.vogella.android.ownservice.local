7452447b4b89ba4a26babc74af3a37a51c042e6e
Itay Nave <itaynave@gmail.com>
2013-03-21 20:05:25 +0200

    Sample widget

--- gen/in/proctracker/android/com/R.java
+++ gen/in/proctracker/android/com/R.java
@@ -13,21 +13,29 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
         public static final int icon1=0x7f020001;
+        public static final int myshape=0x7f020002;
     }
     public static final class id {
-        public static final int button1=0x7f050002;
-        public static final int icon=0x7f050000;
-        public static final int label=0x7f050001;
+        public static final int button1=0x7f060003;
+        public static final int grid_view=0x7f060002;
+        public static final int icon=0x7f060000;
+        public static final int label=0x7f060001;
+        public static final int update=0x7f060004;
     }
     public static final class layout {
         public static final int customtext=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int gridlayout=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int widget_layout=0x7f030003;
     }
     public static final class string {
-        public static final int Button=0x7f040003;
-        public static final int app_name=0x7f040001;
-        public static final int desc=0x7f040004;
-        public static final int hello=0x7f040000;
-        public static final int service_name=0x7f040002;
+        public static final int Button=0x7f050003;
+        public static final int app_name=0x7f050001;
+        public static final int desc=0x7f050004;
+        public static final int hello=0x7f050000;
+        public static final int service_name=0x7f050002;
+    }
+    public static final class xml {
+        public static final int widget_info=0x7f040000;
     }
 }
